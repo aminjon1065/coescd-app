5244ffe6241939d538e83f2b36e91cdff645bbf3
aMiNjOn1065 <aminjon1065@gmail.com>
2025-05-03 15:09:12 +0500

    0.0.1

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,6 +1,16 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
   email: string;
+
+  @MinLength(6, {
+    message: 'Password is too short. Minimum length is 6 characters.',
+  })
   password: string;
+
+  @IsString()
   name: string;
+
   createdAt?: Date;
 }
--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -15,4 +15,7 @@ export class User {
 
   @Column()
   createdAt?: Date;
+
+  @Column()
+  updatedAd?: Date;
 }
